855d9838d3c9392498de8f4840e2bb19061d5102
Darshan G K <122042809+imDarshanGK@users.noreply.github.com>
2023-09-12 22:02:54 +0530

    Problem (Make Avg) - Solve the complete problem
    Problem (Make Avg) - Solve the complete problem

---
+++ Problem (Make Avg) - Solve the complete problem.py
@@ -0,0 +1,15 @@
+# Update the program below to solve the problem
+
+t = int(input())            
+for i in range(t):          
+    A, C = map(int, input().split())
+    
+    #Borrow the solution template from the sub-components
+    if A%2 == 0 and C%2 == 0:
+        B = (A + C)//2
+        print(B)
+    elif A%2 != 0 and C%2 != 0:
+        B = (A + C)//2
+        print(B)
+    else:
+        print(-1)
